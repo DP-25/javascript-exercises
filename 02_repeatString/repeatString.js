edaa27da7a36ebab9425360df754f13684a13061
DP-25 <pedrozadavid68@gmail.com>
2024-10-14 20:49:36 -0700

    Finished 02, passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let temp = "";
+    for(i = 0; i < num;i++){
+        temp += word;
+    }
+    return temp;
 };
 
 // Do not edit below this line
