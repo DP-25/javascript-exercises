5ea9f3abc06d42c1b90c0845de2caf87e4bbac62
DP-25 <pedrozadavid68@gmail.com>
2024-10-15 20:48:22 -0700

    Finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...rest) {
+    let args = rest;
+    let finalArr = arr;
+    let arrLen = finalArr.length;
+    for(let i = 0; i < rest.length;i++){
+        let temp = arrLen;
+        while(temp > 0){
+            if(finalArr.includes(rest[i])){
+                let index = finalArr.findLastIndex(x => x === rest[i]);
+                finalArr.splice(index,1);
+            }
+            temp--;
+        }
+    }
+    return finalArr;
+}; 
 
 // Do not edit below this line
 module.exports = removeFromArray;
