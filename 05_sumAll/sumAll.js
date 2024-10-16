9b022f9bd7ea8f59bca7bc6492c33ed5ccf0d27a
DP-25 <pedrozadavid68@gmail.com>
2024-10-15 21:26:06 -0700

    Finised 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!(Number.isInteger(a)) || !(Number.isInteger(b)) || a < 0 || b < 0){
+        return 'ERROR';
+    }
+    let max = Math.max(a,b);
+    let min = Math.min(a,b);
+    let total = 0;
+    while(max >= min){
+        total += max;
+        max--;
+    }
+    return total;
 };
 
 // Do not edit below this line
